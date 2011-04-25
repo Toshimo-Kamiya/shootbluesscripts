317b7118ac287591d60f3a555b4cc3b0d9e21010
Toshimo-Kamiya <timothy.mahone@gmail.com>
2011-04-25 01:14:27 -0400

    New script: AutoModule.Script.py Purpose: Allows users to configure "always on" modules such as hardeners or ECCM. Interface: None.  Users add module groups directly to ModuleGroupNames array in script.
    Signed-off-by: Toshimo-Kamiya <timothy.mahone@gmail.com>

---
+++ AutoModule/AutoModule.Script.py
@@ -0,0 +1,50 @@
+﻿import shootblues
+from shootblues.common import log
+from shootblues.common.eve import SafeTimer, getFlagName, getNamesOfIDs, findModules, getModuleAttributes, getTypeAttributes, activateModule, canActivateModule, isModuleActive, runOnMainThread
+from shootblues.common.service import forceStart, forceStop
+import service
+import json
+import base
+import uix
+import blue
+import foo
+import math
+import uthread
+
+serviceInstance = None
+serviceRunning = False
+
+ModuleGroupNames = [
+    "ECCM", "Sensor Booster", 
+    "Damage Control"
+]
+
+class ModuleHelperSvc:
+    def __init__(self):
+        self.disabled = False
+        self.__updateTimer = SafeTimer(250, self.updateModules)
+    
+    def updateModules(self):
+        if self.disabled:
+            self.__updateTimer = None
+            return
+        activeModules = findModules(groupNames=ModuleGroupNames)
+        
+        for moduleID, module in activeModules.items():
+            if (canActivateModule(module) and (not isModuleActive(module))):
+                activateModule(module, pulse=True)
+    
+    
+def __load__():
+    global serviceRunning, serviceInstance
+    serviceRunning = True
+    serviceInstance = forceStart("modulehelper", ModuleHelperSvc)
+
+def __unload__():
+    global serviceRunning, serviceInstance
+    if serviceInstance:
+        serviceInstance.disabled = True
+        serviceInstance = None
+    if serviceRunning:
+        forceStop("modulehelper")
+        serviceRunning = False
